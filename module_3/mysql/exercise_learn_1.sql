d74579abbb9a9944603c3655f619a9661926a172
tranviethuy2199 <huytran211998@gmail.com>
2022-09-12 09:01:20 +0700

    nop bai tap

--- module_3/mysql/exercise_learn_1.sql
+++ module_3/mysql/exercise_learn_1.sql
@@ -1,4 +1,11 @@
-CREATE TABLE `class` (
-  `id` int NOT NULL,
-  `name` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`));
+-- SELECT * FROM data_learn_1.teacher;
+-- INSERT INTO teacher (id, name, age, country)
+-- VALUES ('1','HUY','18','DA NANG')
+
+-- SELECT * FROM data_learn_1.teacher;  
+-- INSERT INTO teacher (id, name, age, country)
+-- VALUES ('2','AN','18','HA NOI')
+SELECT * FROM data_learn_1.teacher;
+
+
+
--- module_3/mysql/exercise_learn_1_teacher.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `teacher` (
-  `id` int NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `age` varchar(45) NOT NULL,
-  `country` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
