0c729db0c2b0b13a16f27c74bf438393906131c4
tranviethuy2199 <huytran211998@gmail.com>
2022-09-14 22:22:44 +0700

    nop bai tap

---
+++ module_3/mysql/quanLySinhVien_Ham.sql
@@ -0,0 +1,63 @@
+CREATE database quanLySinhVien;
+USE quanLySinhVien;
+
+CREATE TABLE class(
+class_id int auto_increment primary key not null,
+class_name varchar(60),
+start_date date,
+`status` bit);
+
+CREATE TABLE student(
+student_id int auto_increment primary key not null,
+student_name varchar(50) not null,
+address varchar(50),
+phone varchar(50),
+`status` bit,
+class_id int NOT NULL,
+foreign key(class_id) references class(class_id));
+
+CREATE TABLE subject(
+sub_id int auto_increment primary key not null,
+sub_name varchar(30) not null,
+creadit int not null default 1 check (creadit >= 1),
+status bit default 1);
+
+CREATE TABLE mark(
+mark_id int auto_increment primary key not null,
+sub_id int not null ,
+student_id int not null,
+mark float default 0 check (mark between 0 and 100) ,
+exam_times tinyint default 1,
+unique (sub_id , student_id),
+foreign key(sub_id) references subject(sub_id),
+foreign key(student_id) references student(student_id));
+
+
+INSERT INTO class
+VALUES (1, 'A1', '2008-12-20', 1);
+INSERT INTO Class
+VALUES (2, 'A2', '2008-12-22', 1);
+INSERT INTO Class
+VALUES (3, 'B3', current_date, 0);
+
+INSERT INTO student (student_name, address, phone, `status`, class_id)
+VALUES ('Hung', 'Ha Noi', '0912113113', 1, 1);
+INSERT INTO student (student_name, address, `status`, class_id)
+VALUES ('Hoa', 'Hai phong', 1, 1);
+INSERT INTO student (student_name, address, phone, `status`, class_id)
+VALUES ('Manh', 'HCM', '0123123123', 0, 2);
+
+INSERT INTO subject
+VALUES (1, 'CF', 5, 1),
+       (2, 'C', 6, 1),
+       (3, 'HDJ', 5, 1),
+       (4, 'RDBMS', 10, 1);
+       
+INSERT INTO mark (sub_id, student_id, mark, exam_times)
+VALUES (1, 1, 8, 1),
+       (1, 2, 10, 2),
+       (3, 1, 12, 1);
+SELECT sub_name FROM subject where creadit = 10 ;
+-- SELECT  MAX(sub_name) FROM subject where creadit = 10 ;
+
+SELECT sub_name FROM subject join mark on subject.sub_id = mark.sub_id where mark.mark = 12; 
