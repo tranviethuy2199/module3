d3a02cacf3ee0bb30833f7d55982c28e80923f65
tranviethuy2199 <huytran211998@gmail.com>
2022-09-18 15:11:20 +0700

    nop bai tap

--- module_3/mysql/cas.sql
+++ module_3/mysql/cas.sql
@@ -215,6 +215,8 @@ WHERE dich_vu.ma_dich_vu IN (SELECT hop_dong.ma_dich_vu FROM hop_dong WHERE hop_
  -- cách 2 : sử dụng UNION 2 lệnh select phải có cùng số biêu thức , trả về kết quả không trùng lặp 
  SELECT khach_hang.ho_ten FROM khach_hang UNION SELECT khach_hang.ho_ten FROM khach_hang;
  
+ 
+ 
  -- yêu cầu 9 
  SELECT month(ngay_lam_hop_dong) as doanh_thu_theo_thang , COUNT(ma_hop_dong) FROM hop_dong WHERE YEAR(ngay_lam_hop_dong) = '2021' GROUP BY doanh_thu_theo_thang;
  
@@ -260,8 +262,64 @@ JOIN loai_dich_vu ON loai_dich_vu.ma_loai_dich_vu = dich_vu.ma_loai_dich_vu
  GROUP BY nhan_vien.ma_nhan_vien
  HAVING count(hop_dong.ma_nhan_vien) <= 3;
 
- 
+-- yêu cầu  16
 
 
+DELETE FROM nhan_vien
+WHERE NOT EXISTS(SELECT ma_nhan_vien FROM hop_dong
+WHERE ma_nhan_vien = nhan_vien.ma_nhan_vien AND year(hop_dong.ngay_lam_hop_dong) BETWEEN 2019 AND 2021);
+SELECT ma_nhan_vien, ho_ten FROM nhan_vien;
+
+-- yêu cầu 17
+
+DROP VIEW IF EXISTS demo2;
+CREATE VIEW demo2 AS
+SELECT khach_hang.ma_khach_hang as ma_khach_hang,khach_hang.ho_ten,loai_khach.ten_loai_khach, hop_dong.ma_hop_dong, dich_vu.ten_dich_vu, hop_dong.ngay_lam_hop_dong,hop_dong.ngay_ket_thuc, (ifnull(dich_vu.chi_phi_thue,0)+ SUM(ifnull(hop_dong_chi_tiet.so_luong,0)*ifnull(dich_vu_di_kem.gia,0))) as tong_tien
+FROM khach_hang
+LEFT JOIN loai_khach ON khach_hang.ma_loai_khach = loai_khach.ma_loai_khach 
+LEFT JOIN hop_dong ON khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+LEFT JOIN hop_dong_chi_tiet ON hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong 
+LEFT JOIN dich_vu ON hop_dong.ma_dich_vu = dich_vu.ma_dich_vu 
+LEFT JOIN dich_vu_di_kem ON dich_vu_di_kem.ma_dich_vu_di_kem = hop_dong_chi_tiet.ma_dich_vu_di_kem 
+GROUP BY hop_dong.ma_hop_dong
+ORDER BY khach_hang.ma_khach_hang;
+
+UPDATE khach_hang 
+JOIN demo2 on khach_hang.ma_khach_hang = demo2.ma_khach_hang
+JOIN hop_dong ON hop_dong.ma_khach_hang = khach_hang.ma_khach_hang
+SET khach_hang.ma_loai_khach =1
+WHERE khach_hang.ma_loai_khach=2 AND khach_hang.ma_khach_hang IN (SELECT demo2.ma_khach_hang FROM demo2 WHERE demo2.tong_tien >1000000
+) AND YEAR(hop_dong.ngay_lam_hop_dong)=2021; 
+SELECT * FROM khach_hang;
+
+-- yêu cầu 18.
+SET FOREIGN_KEY_CHECKS=0;
+DELETE khach_hang FROM khach_hang
+INNER JOIN hop_dong ON khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+WHERE hop_dong.ngay_lam_hop_dong IN (SELECT hop_dong.ngay_lam_hop_dong FROM hop_dong
+WHERE year(hop_dong.ngay_lam_hop_dong)<2021) AND hop_dong.ngay_lam_hop_dong NOT IN (SELECT hop_dong.ngay_lam_hop_dong FROM hop_dong
+WHERE year(hop_dong.ngay_lam_hop_dong)>2021);
+SELECT * FROM khach_hang;
+SET FOREIGN_KEY_CHECKS=1;
+
+-- yêu cầu 19
+DROP VIEW IF EXISTS view3;
+CREATE VIEW view3 AS
+SELECT hop_dong_chi_tiet.ma_dich_vu_di_kem, SUM(hop_dong_chi_tiet.so_luong) as so_luong FROM hop_dong_chi_tiet
+JOIN hop_dong ON hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+WHERE hop_dong.ma_hop_dong NOT IN (SELECT hop_dong.ma_hop_dong FROM hop_dong 
+WHERE year(hop_dong.ngay_lam_hop_dong)>=2021)
+GROUP BY hop_dong_chi_tiet.ma_dich_vu_di_kem;
+UPDATE dich_vu_di_kem 
+JOIN view3 ON view3.ma_dich_vu_di_kem =  dich_vu_di_kem.ma_dich_vu_di_kem
+SET dich_vu_di_kem.gia = dich_vu_di_kem.gia*2
+WHERE view3.so_luong>10;
+SELECT * FROM dich_vu_di_kem;
+
+
+-- yêu cầu 20
+SELECT 'nhan vien' as type, ma_nhan_vien as id, ho_ten, email, so_dien_thoai, ngay_sinh, dia_chi FROM nhan_vien
+UNION ALL
+SELECT 'khach hang' as type, ma_khach_hang as id, ho_ten, email, so_dien_thoai, ngay_sinh, dia_chi FROM khach_hang;
  
 
